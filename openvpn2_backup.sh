8a07afc2f18b20b39d31906280e7d9b10fd9f5ec
Nurlan199206 <user_1771@mail.ru>
2017-09-23 13:56:18 +0600

    Update openvpn2_backup.sh

--- openvpn2_backup.sh
+++ openvpn2_backup.sh
@@ -1,22 +1,24 @@
 #!bin/bash
 fold=$(date +%Y%m%d)
+time=$(date +%T%)
 rsync=100
-/usr/bin/expect -f ask
+/usr/bin/expect -f /home/nurlan/ask
 sleep 25
 if
-mount -t cifs //10.8.0.1/data/ /mnt/1C/ -o dom=WORKGROUP,user=user,pass=password
-then echo "remote share \\data on 10.8.0.1 mounted"
-else echo "not mounted"
+/sbin/mount.cifs //10.8.0.1/data/ /mnt/1C/ -o dom=WORKGROUP,user=test,pass=test
+then echo "remote share \\data on 10.8.0.1 mounted at $time"
+else echo "remote share on 10.8.0.1 not mounted at $time" && ps -ef | grep openvpn | grep -v grep | awk '{print $2}' | xargs kill
 exit 0
 fi
 sleep 10
 if
-mount -t cifs //172.27.1.50/1C/ /mnt/nas -o dom=WORKGROUP,user=user,pass=password,vers=2.0
-then echo "NAS remote share folder mounted..."
+mount -t cifs //172.27.1.50/1C/ /mnt/nas -o dom=WORKGROUP,user=test,pass=test,vers=2.0
+then echo "NAS remote share folder mounted at $time"
 fi
-rsync -rPzv --progress /mnt/1C/CCG/base/ /mnt/nas/$fold
+sleep 10
+rsync -av --progress /mnt/1C/CCG/base/ /mnt/nas/$fold
 if [ "$rsync" -eq "100" ]
-then echo "backed up successfully... unmounting remote shares /mnt/nas and /mnt/1C" && umount -l /mnt/nas && umount -l /mnt/1C
+then echo "backed up successfully... unmounting remote shares /mnt/nas and /mnt/1C and terminating OpenVPN connection at $time" && umount -l /mnt/nas && umount -l /mnt/1C
 else
 echo "Backup failed..."
 fi
