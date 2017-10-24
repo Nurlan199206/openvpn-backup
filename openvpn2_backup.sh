5aa516dd3918f5509c3417be4378b41e484591ff
Nurlan199206 <user_1771@mail.ru>
2017-10-24 16:43:31 +0600

    Update openvpn2_backup.sh

--- openvpn2_backup.sh
+++ openvpn2_backup.sh
@@ -5,20 +5,20 @@ rsync=100
 /usr/bin/expect -f /home/nurlan/ask
 sleep 25
 if
-/sbin/mount.cifs //10.8.0.1/data/ /mnt/1C/ -o dom=WORKGROUP,user=ccg5,pass=63jyNd2d
+/sbin/mount.cifs //10.8.0.1/data/ /mnt/1C/ -o dom=WORKGROUP,user=test,pass=test
 then echo "remote share \\data on 10.8.0.1 mounted at $time \n"
 else echo "remote share on 10.8.0.1 not mounted at $time \n" && ps -ef | grep openvpn | grep -v grep | awk '{print $2}' | xargs kill
 exit 0
 fi
 sleep 10
 if
-mount -t cifs //172.27.1.50/1C/ /mnt/nas -o dom=WORKGROUP,user=nurlan,pass=a982lsnn,vers=2.0
+mount -t cifs //172.27.1.50/1C/ /mnt/nas -o dom=WORKGROUP,user=test,pass=test,vers=2.0
 then echo "NAS remote share folder mounted at $time \n"
 fi
 sleep 10
 rsync -av --progress /mnt/1C/CCG/base/ /mnt/nas/$fold
 if [ "$rsync" -eq "100" ]
-then echo "backed up successfully... unmounting remote shares /mnt/nas and /mnt/1C and terminating OpenVPN connection at $time \n" && umount -l /mnt/nas && umount -l /mnt/1C && echo "1C Backup successfull" | mail -s "Success" nurlan.izimbetov@ccg.com.kz
+then echo "backed up successfully... unmounting remote shares /mnt/nas and /mnt/1C and terminating OpenVPN connection at $time \n" && umount -l /mnt/nas && umount -l /mnt/1C && echo "1C Backup successfull" | mail -s "Success" test@mail.com
 else
 echo "Backup failed... \n"
 fi
